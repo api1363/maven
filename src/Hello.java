7d79f0a4ffe0033d46e38bf9840fa4ad3e2c87dd
api1363 <87800411+api1363@users.noreply.github.com>
2021-07-28 14:57:01 +0530

    Update Hello.java

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,7 @@
 
 public class Hello {
 public static void main(String[] args) {
-	System.out.println("Hello from  Pritam");
+	System.out.println("Hello from Pritam");
 	System.out.println("Hello from Diksha  Batch 20");
 }
 }
